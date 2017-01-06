c5ccd64c4c7d324737139b1fe556d08feebfcf98
SYNAPSE\sumit.bhardwaj <17.sumit.bhardwaj@gmail.com>
2017-01-06 10:23:07 +0530

    First Commit

---
+++ com.globallogic.sourceTreeDemo/src/test/java/com/globallogic/files/TestClass.java
@@ -0,0 +1,17 @@
+package com.globallogic.files;
+
+public class TestClass {
+	
+	private String Variable1 ="";
+	private String Variable2="";
+	
+	
+	public void method1(String var){
+		this.Variable1=var;
+	}
+	
+	public void method2(String var2){
+		this.Variable2=var2;
+	}
+
+}
